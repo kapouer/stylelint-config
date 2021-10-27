bcd064d388315453205071f99f1dae5203183337
Jérémy Lal <kapouer@melix.org>
2021-10-28 01:11:41 +0200

    Leave class and id patterns to us

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
 	extends: 'stylelint-config-standard',
 	rules: {
+		"selector-class-pattern": null,
+		"selector-id-pattern": null,
 		"block-closing-brace-empty-line-before": [
 			"never", { "severity": "warning" }
 		],
